2fc7e96e6740dee480e39a9def244d7bfc209b28
a.kovalenko <you@example.com>
2019-06-20 03:14:45 +0300

    First commit: add project.

---
+++ capacity.js
@@ -0,0 +1,3 @@
+const P = [0, 10, 15, 25, 30, 35, 5];
+
+module.exports = P;
---
+++ index.js
@@ -0,0 +1,133 @@
+'use strict';
+
+const A = require('./distanse-matrix');
+const P = require('./capacity');
+const routesList = require('./genetic/individual-static');
+
+const Q = 50;           //constant for vehicle capacity
+const D = 15;           //population size
+const CUSTOMERS = 6;    //number of customers
+const ITERATIONS = 10;   //number of iterations
+
+const random = (min, max) => {
+    max += 1;
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const compare = (individ1, individ2) => {
+    return individ2.val - individ1.val;
+};
+
+//function for generating random route
+const createRoute = () => {
+    const route = [];
+    return route;   //return route array
+};
+
+const newIndivid = (path) => {
+    const cost = routeCost(path);
+    return {
+        val: cost,
+        path: path
+    };
+};
+
+const createPopulation = () => {
+    let population = [];
+    for(let i = 0; i < D; i++){
+        const route = createRoute();
+        const individ = newIndivid(route);
+        population.push(individ);
+    }
+    population.sort(compare);
+};
+
+//route - array
+const routeCost = (route) => {
+    let sum = 0;
+    let capacity = Q;
+    let path = '';
+
+    for(let i = 0; i < route.length; i++){
+        capacity -= P[route[i]];
+        if(capacity >= P[route[i+1]]){
+            path += route[i];
+            sum += A[route[i]][route[i+1]];
+        }
+        else{
+            path += route[i];
+            path += '0';
+            sum += A[route[i]][0];
+            if(i !== route.length - 1) {
+                sum += A[0][route[i + 1]];
+            }
+            capacity = 50;
+        }
+        // console.log('=============');
+    }
+    console.log(path);
+    return sum;
+};
+
+const crossover = (path1, path2) => {
+    let child = [];
+    for(let i = 0; i < path1.length; i++){
+        child[path1[i]] = path2[i];
+    }
+    return child;
+};
+
+const mutation = (path, k) => {
+    let arr1 = path.splice(0, k);
+    let arr2 = path.splice(k+1, path.length);
+    arr1 = arr1.reverse();
+    return [...arr1, ...arr2];
+};
+
+const selection = (population) => {
+    population.sort(compare);
+    while(population.length > D){
+        population.pop();
+    }
+    return population;
+};
+
+const iteration = (population) => {
+    const k1 = random(0,population.length);
+    const k2 = random(0,population.length);
+    const k3 = random(0, population.length);
+    const k4 = random(0, population.length);
+    const k5 = random(0, CUSTOMERS);
+
+    const parent1 = population[k1].path;
+    const parent2 = population[k2].path;
+    const child = crossover(parent1, parent2);
+    const crossoverChild = newIndivid(child);
+    population.push(crossoverChild);
+
+    for(let i = 0; i < population.length; i++)
+    {
+        if(i % k4 === 0){
+            const mutable = population[k3].path;
+            const child = mutation(mutable, k5);
+            const mutationChild = newIndivid(child);
+            population.push(mutationChild);
+        }
+    }
+
+    const newPopulation = selection(population);
+    return newPopulation;
+};
+
+const main = () => {
+    // let population = createPopulation();
+    let population = routesList;
+    for(let i = 0; i < ITERATIONS; i++){
+        population = iteration(population);
+    }
+    console.log("The best found way: ", population[0].path);
+    console.log("The value of way: ", population[0].val);
+};
+
+main();
+
